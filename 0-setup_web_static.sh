b9753d682096259f1a65e01e2c8e29c133cd2c99
NaomiKleinhans <naomikleinhans56@gmail.com>
2024-07-14 08:31:46 -0700

    fixes

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -10,7 +10,7 @@ sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
 
 # Create a fake HTML file
-echo "Hello!" | sudo tee /data/web_static/releases/test/index.html
+sudo tee /data/web_static/releases/test/index.html
 
 # Create a symbolic link
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
@@ -24,4 +24,3 @@ sudo sed -i '/server_name _;/a location /AirBnb_static/ {\n\talias /data/web_sta
 # Restart Nginx
 sudo service nginx restart
 
-exit 0
