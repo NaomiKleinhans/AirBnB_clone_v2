b945f67647ebf6c0d6edcb6f58c6bf702a72e4e6
NaomiKleinhans <naomikleinhans56@gmail.com>
2024-08-11 00:57:14 +0200

    Update 2-c_route.py

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -5,7 +5,8 @@ This module starts a Flask web application with three routes.
 The application listens on 0.0.0.0:5000 and has the following routes:
 - '/' which returns the message "Hello HBNB!"
 - '/hbnb' which returns the message "HBNB"
-- '/c/<text>' which returns the string "C " followed by the value of `<text>`, with underscores replaced by spaces
+- '/c/<text>' which returns the string "C " followed by the value of `<text>`,
+  with underscores replaced by spaces.
 
 This module demonstrates basic Flask route handling with dynamic URL parameters.
 """
@@ -43,10 +44,12 @@ def c_is_fun(text):
     Handle the route '/c/<text>' where <text> is a dynamic URL parameter.
 
     Args:
-        text (str): The text to be displayed after "C ". Underscores in the text are replaced by spaces.
+        text (str): The text to be displayed after "C ". Underscores in the text
+                    are replaced by spaces.
 
     Returns:
-        str: The string "C " followed by the value of <text> with underscores replaced by spaces.
+        str: The string "C " followed by the value of <text> with underscores
+             replaced by spaces.
     """
     return "C " + text.replace('_', ' ')
 
