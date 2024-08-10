9005bd58dce76534c78cdd817f60e2f84c86631a
NaomiKleinhans <naomikleinhans56@gmail.com>
2024-08-11 00:49:59 +0200

    progress

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,12 +1,29 @@
 #!/usr/bin/python3
+"""
+This module starts a simple Flask web application.
+
+The application listens on 0.0.0.0:5000 and has a single route:
+- '/' which returns the message "Hello HBNB!".
+"""
+
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """
+    Return the message 'Hello HBNB!' when accessing the root route.
+
+    Returns:
+        str: A simple greeting message.
+    """
     return "Hello HBNB!"
 
+
 if __name__ == "__main__":
+    """
+    Run the Flask application on host 0.0.0.0 and port 5000.
+    """
     app.run(host="0.0.0.0", port=5000)
-    
