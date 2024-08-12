92ebb193ec090d3ce9e01ae36516348cc0f90eb2
NaomiKleinhans <naomikleinhans56@gmail.com>
2024-08-12 18:01:30 +0200

    progress

--- models/base_model.py
+++ models/base_model.py
@@ -12,47 +12,38 @@ from models import storage
 
 
 class BaseModel:
+    """Class from which all other classes will inherit"""
 
- """Class from which all other classes will inherit"""
-
-def __init__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
         """Initializes instance attributes
         Args:
             - *args: list of arguments
             - **kwargs: dict of key-values arguments
         """
-
-        if kwargs is not None and kwargs != {}:
-            for key in kwargs:
-                if key == "created_at":
-                    self.__dict__["created_at"] = datetime.strptime(
-                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
-                elif key == "updated_at":
-                    self.__dict__["updated_at"] = datetime.strptime(
-                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    self.__dict__[key] = datetime.strptime(
+                        value, "%Y-%m-%dT%H:%M:%S.%f")
                 else:
-                    self.__dict__[key] = kwargs[key]
+                    self.__dict__[key] = value
         else:
-            self.id = str(uuid.uuid4())
+            self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             storage.new(self)
 
-def __str__(self):
+    def __str__(self):
         """Returns official string representation"""
+        return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
 
-        return "[{}] ({}) {}".\
-            format(type(self).__name__, self.id, self.__dict__)
-
-def save(self):
-        """updates the public instance attribute updated_at"""
-
+    def save(self):
+        """Updates the public instance attribute updated_at"""
         self.updated_at = datetime.now()
         storage.save()
 
-def to_dict(self):
-        """returns a dictionary containing all keys/values of __dict__"""
-
+    def to_dict(self):
+        """Returns a dictionary containing all keys/values of __dict__"""
         my_dict = self.__dict__.copy()
         my_dict["__class__"] = type(self).__name__
         my_dict["created_at"] = my_dict["created_at"].isoformat()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -6,27 +6,30 @@ import os
 
 
 class FileStorage:
-
     """Class for storing and retrieving data"""
     __file_path = "file.json"
     __objects = {}
 
-    def all(self):
-        """returns the dictionary __objects"""
-        return FileStorage.__objects
+    @classmethod
+    def all(cls):
+        """Returns the dictionary __objects"""
+        return cls.__objects
 
-    def new(self, obj):
-        """sets in __objects the obj with key <obj class name>.id"""
+    @classmethod
+    def new(cls, obj):
+        """Sets in __objects the obj with key <obj class name>.id"""
         key = "{}.{}".format(type(obj).__name__, obj.id)
-        FileStorage.__objects[key] = obj
+        cls.__objects[key] = obj
 
-    def save(self):
-        """ serializes __objects to the JSON file (path: __file_path)"""
-        with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
-            d = {k: v.to_dict() for k, v in FileStorage.__objects.items()}
+    @classmethod
+    def save(cls):
+        """Serializes __objects to the JSON file (path: __file_path)"""
+        with open(cls.__file_path, "w", encoding="utf-8") as f:
+            d = {k: v.to_dict() for k, v in cls.__objects.items()}
             json.dump(d, f)
 
-    def classes(self):
+    @staticmethod
+    def classes():
         """Returns a dictionary of valid classes and their references"""
         from models.base_model import BaseModel
         from models.user import User
@@ -36,64 +39,73 @@ class FileStorage:
         from models.place import Place
         from models.review import Review
 
-        classes = {"BaseModel": BaseModel,
-                   "User": User,
-                   "State": State,
-                   "City": City,
-                   "Amenity": Amenity,
-                   "Place": Place,
-                   "Review": Review}
-        return classes
-
-    def reload(self):
-        """Reloads the stored objects"""
-        if not os.path.isfile(FileStorage.__file_path):
-            return
-        with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
-            obj_dict = json.load(f)
-            obj_dict = {k: self.classes()[v["__class__"]](**v)
-                        for k, v in obj_dict.items()}
-            # TODO: should this overwrite or insert?
-            FileStorage.__objects = obj_dict
-
-    def attributes(self):
-        """Returns the valid attributes and their types for classname"""
-        attributes = {
-            "BaseModel":
-                     {"id": str,
-                      "created_at": datetime.datetime,
-                      "updated_at": datetime.datetime},
-            "User":
-                     {"email": str,
-                      "password": str,
-                      "first_name": str,
-                      "last_name": str},
-            "State":
-                     {"name": str},
-            "City":
-                     {"state_id": str,
-                      "name": str},
-            "Amenity":
-                     {"name": str},
-            "Place":
-                     {"city_id": str,
-                      "user_id": str,
-                      "name": str,
-                      "description": str,
-                      "number_rooms": int,
-                      "number_bathrooms": int,
-                      "max_guest": int,
-                      "price_by_night": int,
-                      "latitude": float,
-                      "longitude": float,
-                      "amenity_ids": list},
-            "Review":
-            {"place_id": str,
-                         "user_id": str,
-                         "text": str}
+        return {
+            "BaseModel": BaseModel,
+            "User": User,
+            "State": State,
+            "City": City,
+            "Amenity": Amenity,
+            "Place": Place,
+            "Review": Review
         }
-        return attributes
-    
-    def close(self):
+
+    @classmethod
+    def reload(cls):
+        """Reloads the stored objects"""
+        if not os.path.isfile(cls.__file_path):
+            return
+        with open(cls.__file_path, "r", encoding="utf-8") as f:
+            obj_dict = json.load(f)
+            obj_dict = {k: cls.classes()[v["__class__"]](**v)
+                        for k, v in obj_dict.items()}
+            cls.__objects = obj_dict
+
+    @staticmethod
+    def attributes():
+        """Returns the valid attributes and their types for classname"""
+        return {
+            "BaseModel": {
+                "id": str,
+                "created_at": datetime.datetime,
+                "updated_at": datetime.datetime
+            },
+            "User": {
+                "email": str,
+                "password": str,
+                "first_name": str,
+                "last_name": str
+            },
+            "State": {
+                "name": str
+            },
+            "City": {
+                "state_id": str,
+                "name": str
+            },
+            "Amenity": {
+                "name": str
+            },
+            "Place": {
+                "city_id": str,
+                "user_id": str,
+                "name": str,
+                "description": str,
+                "number_rooms": int,
+                "number_bathrooms": int,
+                "max_guest": int,
+                "price_by_night": int,
+                "latitude": float,
+                "longitude": float,
+                "amenity_ids": list
+            },
+            "Review": {
+                "place_id": str,
+                "user_id": str,
+                "text": str
+            }
+        }
+
+    @classmethod
+    def close(cls):
         """Call the reload method to deserialize the JSON file to objects."""
-        self.reload()
+        cls.reload()
