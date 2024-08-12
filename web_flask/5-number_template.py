e04dd5c2825c99a6876d699a65b974dd956430f2
NaomiKleinhans <naomikleinhans56@gmail.com>
2024-08-12 16:38:29 +0200

    Update 5-number_template.py

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -4,7 +4,8 @@ This script starts a Flask web application with various routes:
 - /: displays "Hello HBNB!"
 - /hbnb: displays "HBNB"
 - /c/<text>: displays "C " followed by the value of the text variable
-- /python/(<text>): displays "Python " followed by the value of the text variable
+- /python/(<text>): displays "Python " followed by the value of the 
+text variable
 - /number/<n>: displays "n is a number" only if n is an integer
 - /number_template/<n>: displays a HTML page only if n is an integer
 """
